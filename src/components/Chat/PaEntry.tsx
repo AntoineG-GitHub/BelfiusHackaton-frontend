691876523da8c2dcd5bc9b5d10e82b0ba449d4c2
Brecht Luts <brecht.luts@gmail.com>
2025-11-29 11:43:22 +0100

    PowR UP

--- src/components/Chat/PaEntry.tsx
+++ src/components/Chat/PaEntry.tsx
@@ -30,7 +30,7 @@ export function PaEntry() {
     <div className="absolute bg-white flex items-center gap-[15px] left-[170px] top-[35px] w-[364px] h-[112px]">
       <ChatAvatar />
       <div className="flex-1 text-[36px] font-['Belfius21:Regular'] leading-[44px]">
-        PowerUp
+        PowR UP
       </div>
     </div>
   );
